0b397ae0f586e2c32fa519f1e9a311d479550cae
echocall <pamela.lynn.pepper@gmail.com>
2023-09-18 19:12:51 -0400

    Update readJSON.ph
    readded Jacob's code in readJSON.php.
     Original merge from my branch to master did not detect collisions in this file but apparently overwrote it all anyways. :|

--- lib/readJSON.php
+++ lib/readJSON.php
@@ -1,1 +1,11 @@
 <?php
+function readJson($filename) {
+    if (!file_exists($filename)) {
+        return "File not found!";
+    }
+    $fileContent = file_get_contents($filename);
+
+    $data = json_decode($fileContent, true);
+
+    return $data;
+}
